6edf6c95d521584cb9e290aa4ac23b86f7c30a1e
Sophie Moller <sophierosemoller@icloud.com>
2024-05-04 22:16:31 +1200

    Tower class, Tower manager class, HomePageController

---
+++ src/main/java/seng201/team0/TowerManager.java
@@ -0,0 +1,23 @@
+package seng201.team0;
+
+import seng201.team0.models.Tower;
+
+import java.util.List;
+
+public class TowerManager {
+    private String towerName;
+    private List<Tower> towerList;
+    public void setTowerList(List<Tower> towerList){
+        this.towerList = towerList;
+    }
+    public void setTowerName(String towerName){
+        this.towerName = towerName;
+    }
+
+    public TowerManager(){
+        towerList.addAll(List.of(new Tower("Piglet Palace", 25, "Pigs", 15, 2, 45.00),
+                new Tower("Cowtopia Castle", 10, "Cows", 30, 1,25.00),
+                new Tower("Haybale Haven", 50, "Hay", 10, 1, 10.00),
+                new Tower("Timber Turret", 20, "Timber", 60, 2, 50.00)));
+    }
+}
---
+++ src/main/java/seng201/team0/models/Tower.java
@@ -0,0 +1,42 @@
+package seng201.team0.models;
+
+public class Tower {
+    private String towerName;
+    private int towerResourceAmount;
+    private String towerResourceType;
+    private int towerReloadSpeed;
+    private int towerLevel;
+    private double towerCost;
+    private static final String[] RESOURCE_TYPES = {"Hay", "Water", "Cows", "Pigs", "Timber", "Chickens"};
+
+    public Tower(String towerName, int towerResourceAmount, String towerResourceType, int towerReloadSpeed, int towerLevel, double towerCost){
+        this.towerName = towerName;
+        this.towerResourceAmount = towerResourceAmount;
+        this.towerResourceType = towerResourceType;
+        this.towerReloadSpeed = towerReloadSpeed;
+        this.towerLevel = towerLevel;
+        this.towerCost = towerCost;
+    }
+    public String getTowerName(){
+        return towerName;
+    }
+    public void setTowerName(String towerName){
+        this.towerName = towerName;
+    }
+    public int getTowerResourceAmount(){
+        return towerResourceAmount;
+    }
+    public String getTowerResourceType(){
+        return towerResourceType;
+    }
+    public int getTowerReloadSpeed(){
+        return towerReloadSpeed;
+    }
+    public int getTowerLevel(){
+        return towerLevel;
+    }
+    public double getTowerCost(){
+        return towerCost;
+    }
+
+}
