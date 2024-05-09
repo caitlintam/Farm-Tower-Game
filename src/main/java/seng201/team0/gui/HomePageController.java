78b63fe41b1997d5fb0af2f4f40ff768565f95eb
Sophie Moller <sophierosemoller@icloud.com>
2024-05-09 14:10:09 +1200

    Tower class, Tower manager class, HomePageController

--- src/main/java/seng201/team0/gui/HomePageController.java
+++ src/main/java/seng201/team0/gui/HomePageController.java
@@ -3,37 +3,46 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import seng201.team0.PlayerManager;
+import seng201.team0.TowerManager;
+import seng201.team0.models.Tower;
 import javafx.stage.Stage;
-import seng201.team0.services.CounterService;
 
 public class HomePageController {
+    private TowerManager towerManager;
+    private PlayerManager playerManager;
     @FXML
-    private Label defaultLabel;
+    private Label playerNameLabel;
 
     @FXML
-    private Button defaultButton;
+    private Label homepageMoney;
+    @FXML
+    private Label homepageCurrentRoundLabel;
+    private Label remainingRoundsLabel;
 
-    private CounterService counterService;
+    @FXML
+    private Button goToShopButton;
+    @FXML
+    private Button PlayRoundButton;
+    @FXML
+    private Button viewInventoryButton;
 
-    /**
-     * Initialize the window
-     *
-     * @param stage Top level container for this window
-     */
-    public void init(Stage stage) {
-        counterService = new CounterService();
+
+    public void initialize(){
+        playerNameLabel.setText(PlayerManager.getName());
+        homepageMoney.setText(PlayerManager.getMoney()); // need to find a way get money from player class
+        //homepageCurrentRoundLabel.setText(PlayerManager.getCurrentRound());
+        remainingRoundsLabel.setText(PlayerManager.getNumGameRounds());
+
     }
 
-    /**
-     * Method to call when our counter button is clicked
-     *
-     */
-    @FXML
-    public void onButtonClicked() {
-        System.out.println("Button has been clicked");
-        counterService.incrementCounter();
+    private HomePageController(TowerManager towerManager){
+        this.towerManager = towerManager;
+        updateHomePage();
 
-        int count = counterService.getCurrentCount();
-        defaultLabel.setText(Integer.toString(count));
     }
+    public void updateHomePage(){
+        //
+    }
+
 }
