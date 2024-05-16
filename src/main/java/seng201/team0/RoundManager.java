761fa53613a5ad49db031794ca6ee6ef24801c67
Caitlin Tam <cta108@uclive.ac.nz>
2024-05-16 14:18:35 +1200

    - added new classes - round, cart

---
+++ src/main/java/seng201/team0/RoundManager.java
@@ -0,0 +1,39 @@
+package seng201.team0;
+
+public class RoundManager {
+//    private List<Round> rounds;
+//    private int currentRoundIndex;
+
+//    public void createRoundThreads(List<Round> rounds) {
+//        for (Round round : rounds) {
+//            RoundTask roundTask = new RoundTask(round);
+//            Thread roundThread = new Thread(roundTask);
+//            roundThreads.add(roundThread);
+//        }
+//    }
+//
+//    public void startRoundThreads() {
+//        for (Thread roundThread : roundThreads) {
+//            roundThread.start();
+//        }
+//    }
+//
+//    public void endRoundThreads() {
+//        for (Thread roundThread : roundThreads) {
+//            roundThread.interrupt();
+//        }
+//    }
+//
+//    private class RoundTask implements Runnable {
+//        private Round round;
+//
+//        public RoundTask(Round round) {
+//            this.round = round;
+//        }
+//
+//        @Override
+//        public void run() {
+//            // Logic for processing the round
+//        }
+//    }
+}
